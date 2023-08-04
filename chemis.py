c190553a89db1b63a60e42c3bd857fe44a6a0357
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-08-04 14:34:37 -0500

    Update chemis.py

--- chemis.py
+++ chemis.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import py3Dmol
+import pubchempy as pcp
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import Lipinski, Descriptors
@@ -15,6 +16,8 @@ st.title('RDKit + Py3DMOL 😀')
 
 smiles=st.text_input('SMILES please','CC')
 mol = Chem.MolFromSmiles(smiles)
+img = Draw.MolToImage(mol)
 
 mol_wt = round(Descriptors.MolWt(mol), 4)
 st.write(f'Peso molecular: {mol_wt}')
+st.image(img, use_column_width=True, caption=f'Imagen en 2D de la estructura de la molécula {iupac_name}')
