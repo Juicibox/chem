14ed52a488b9fa1e8e886eba1d6289f8cf4a8b32
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-08-04 14:27:33 -0500

    Update chemis.py

--- chemis.py
+++ chemis.py
@@ -1,8 +1,14 @@
 import streamlit as st
-
 import py3Dmol
 from rdkit import Chem
 from rdkit.Chem import AllChem
+from rdkit.Chem import Lipinski, Descriptors
+from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem import Crippen
+from rdkit.Chem import AllChem
+
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import SimilarityMaps
 
 st.title('RDKit + Py3DMOL 😀')
 
