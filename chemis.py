96aaa709385537bae835ebf002662a1006c2e837
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-08-04 14:44:36 -0500

    Update chemis.py

--- chemis.py
+++ chemis.py
@@ -20,10 +20,7 @@ img = Draw.MolToImage(mol)
 name = "mole"
 
 AllChem.ComputeGasteigerCharges(mol)
-contribs = [mol.GetAtomWithIdx(i).GetDoubleProp('_GasteigerCharge') for i in range(mol.GetNumAtoms())]
-fig = SimilarityMaps.GetSimilarityMapFromWeights(mol, contribs, colorMap='jet', contourLines=10)
-contribs1 = rdMolDescriptors._CalcCrippenContribs(mol)
-fig1 = SimilarityMaps.GetSimilarityMapFromWeights(mol, [x for x, y in contribs1], colorMap='jet',contourLines=10)
+
 
 
 col1, col2 = st.columns(2)
@@ -32,11 +29,6 @@ with col1:
 
   st.image(img, use_column_width=True, caption=f'Imagen en 2D de la estructura de la molécula {name}')
  
-  st.markdown(f"<h2 style='text-align: center; color: #a0a0a0; font-size: 13px;'>Imagen de las cargas parciales de Gasteiger para la molécula {name}</h2>", unsafe_allow_html=True)
-
-  st.markdown(
-      f"<h2 style='text-align: center; color: #a0a0a0; font-size: 13px;'>Imagen de las contribuciones de Crippen para la solubilidad (logP) para la molécula {name}</h2>",
-      unsafe_allow_html=True)
 
 with col2:
   mol_wt = round(Descriptors.MolWt(mol), 4)
