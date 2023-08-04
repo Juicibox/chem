a4219588d4b18b0ac4023de6a5754292de2032a4
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-08-04 15:07:57 -0500

    Update chemis.py

--- chemis.py
+++ chemis.py
@@ -20,59 +20,102 @@ def iupac_to_smiles(iupac_name):
     else:
         return None
 
-smiles=st.text_input('SMILES please','CC')
-mol = Chem.MolFromSmiles(smiles)
-img = Draw.MolToImage(mol)
-name = "mole"
+def visualize_molecule():
+    st.title("Visualización de moléculas")
 
-AllChem.ComputeGasteigerCharges(mol)
-contribs = [mol.GetAtomWithIdx(i).GetDoubleProp('_GasteigerCharge') for i in range(mol.GetNumAtoms())]
-fig = SimilarityMaps.GetSimilarityMapFromWeights(mol, contribs, colorMap='jet', contourLines=10)
+    # Casilla de entrada para el nombre en IUPAC
+    iupac_name = st.text_input("Ingrese el nombre IUPAC en inglés")
+
+    if iupac_name:
+        # Convertir el nombre en IUPAC a SMILES utilizando la función iupac_to_smiles
+        smiles = iupac_to_smiles(iupac_name)
+
+        if smiles:
+            # Visualizar la molécula correspondiente utilizando la función visualize_molecule
+            mol = Chem.MolFromSmiles(smiles)
+            img = Draw.MolToImage(mol)
+
+            AllChem.ComputeGasteigerCharges(mol)
+            contribs = [mol.GetAtomWithIdx(i).GetDoubleProp('_GasteigerCharge') for i in range(mol.GetNumAtoms())]
+            fig = SimilarityMaps.GetSimilarityMapFromWeights(mol, contribs, colorMap='jet', contourLines=10)
+            contribs1 = rdMolDescriptors._CalcCrippenContribs(mol)
+            fig1 = SimilarityMaps.GetSimilarityMapFromWeights(mol, [x for x, y in contribs1], colorMap='jet',contourLines=10)
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+
+                st.image(img, use_column_width=True, caption=f'Imagen en 2D de la estructura de la molécula {iupac_name}')
+                st.pyplot(fig)
+                st.markdown(f"<h2 style='text-align: center; color: #a0a0a0; font-size: 13px;'>Imagen de las cargas parciales de Gasteiger para la molécula {iupac_name}</h2>", unsafe_allow_html=True)
+                st.pyplot(fig1)
+                st.markdown(
+                    f"<h2 style='text-align: center; color: #a0a0a0; font-size: 13px;'>Imagen de las contribuciones de Crippen para la solubilidad (logP) para la molécula {iupac_name}</h2>",
+                    unsafe_allow_html=True)
+
+            with col2:
+                mol_wt = round(Descriptors.MolWt(mol), 4)
+                st.write(f'Peso molecular: {mol_wt}')
+
+                num_atoms = mol.GetNumAtoms()
+                st.write(f'Número de átomos: {num_atoms}')
+
+                num_bonds = mol.GetNumBonds()
+                st.write(f'Número de enlaces: {num_bonds}')
+
+                num_acceptors = Lipinski.NumHAcceptors(mol)
+                st.write(f'Número de aceptores de enlaces de hidrógeno: {num_acceptors}')
+
+                num_donors = Lipinski.NumHDonors(mol)
+                st.write(f'Número de donantes de enlaces de hidrógeno: {num_donors}')
+
+                num_rotatable_bonds = Lipinski.NumRotatableBonds(mol)
+                st.write(f'Número de enlaces giratorios: {num_rotatable_bonds}')
+
+                num_aliphatic_rings = Lipinski.NumAliphaticRings(mol)
+                st.write(f'Número de anillos alifáticos: {num_aliphatic_rings}')
+
+                num_aromatic_rings = Lipinski.NumAromaticRings(mol)
+                st.write(f'Número de anillos aromáticos: {num_aromatic_rings}')
+
+                fraction_csp3 = Lipinski.FractionCSP3(mol)
+                st.write(f'Proporción de átomos de carbono híbrido SP3: {fraction_csp3}')
+
+                inde_flex = round(Crippen.MolMR(mol),4)
+                st.write(f'Índice de refracción molar: {inde_flex}')
+
+                mol_log_p = round(Descriptors.MolLogP(mol),4)
+                st.write(f'Coeficiente de partición grasa-agua: {mol_log_p}')
+
+                tpsa = round(Descriptors.TPSA(mol), 4)
+                st.write(f'Área de superficie del polo topológico: {tpsa}')
 
 
 
-col1, col2 = st.columns(2)
 
-with col1:
 
-  st.image(img, use_column_width=True, caption=f'Imagen en 2D de la estructura de la molécula {name}')
- 
+        else:
+            st.error("No se encontró una molécula para el nombre en IUPAC proporcionado.")
 
-with col2:
-  mol_wt = round(Descriptors.MolWt(mol), 4)
-  st.write(f'Peso molecular: {mol_wt}')
-  
-  num_atoms = mol.GetNumAtoms()
-  st.write(f'Número de átomos: {num_atoms}')
 
-  num_bonds = mol.GetNumBonds()
-  st.write(f'Número de enlaces: {num_bonds}')
 
-  num_acceptors = Lipinski.NumHAcceptors(mol)
-  st.write(f'Número de aceptores de enlaces de hidrógeno: {num_acceptors}')
+def chemical_balancing():
+    st.title("Balanceo")
+    # Aquí puedes agregar tu lógica de balanceo químico
 
-  num_donors = Lipinski.NumHDonors(mol)
-  st.write(f'Número de donantes de enlaces de hidrógeno: {num_donors}')
+def main():
+    # Menú lateral
+    st.sidebar.title("Opciones")
+    selected_option = st.sidebar.selectbox("Seleccione una opción", ["Visualización", "Balanceo químico"])
 
-  num_rotatable_bonds = Lipinski.NumRotatableBonds(mol)
-  st.write(f'Número de enlaces giratorios: {num_rotatable_bonds}')
+    if selected_option == "Visualización":
+        visualize_molecule()
+    elif selected_option == "Balanceo":
+        chemical_balancing()
 
-  num_aliphatic_rings = Lipinski.NumAliphaticRings(mol)
-  st.write(f'Número de anillos alifáticos: {num_aliphatic_rings}')
+if __name__ == '__main__':
+    main()
 
-  num_aromatic_rings = Lipinski.NumAromaticRings(mol)
-  st.write(f'Número de anillos aromáticos: {num_aromatic_rings}')
 
-  fraction_csp3 = Lipinski.FractionCSP3(mol)
-  st.write(f'Proporción de átomos de carbono híbrido SP3: {fraction_csp3}')
-
-  inde_flex = round(Crippen.MolMR(mol),4)
-  st.write(f'Índice de refracción molar: {inde_flex}')
-
-  mol_log_p = round(Descriptors.MolLogP(mol),4)
-  st.write(f'Coeficiente de partición grasa-agua: {mol_log_p}')
-
-  tpsa = round(Descriptors.TPSA(mol), 4)
-  st.write(f'Área de superficie del polo topológico: {tpsa}')
 
 
