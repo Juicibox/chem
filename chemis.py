503c6df541b0be6e5798aa6749955bf092949500
Juan Roncancio <109776558+Juicibox@users.noreply.github.com>
2023-08-04 14:45:22 -0500

    Update chemis.py

--- chemis.py
+++ chemis.py
@@ -33,6 +33,38 @@ with col1:
 with col2:
   mol_wt = round(Descriptors.MolWt(mol), 4)
   st.write(f'Peso molecular: {mol_wt}')
+  
+  num_atoms = mol.GetNumAtoms()
+  st.write(f'Número de átomos: {num_atoms}')
 
+  num_bonds = mol.GetNumBonds()
+  st.write(f'Número de enlaces: {num_bonds}')
+
+  num_acceptors = Lipinski.NumHAcceptors(mol)
+  st.write(f'Número de aceptores de enlaces de hidrógeno: {num_acceptors}')
+
+  num_donors = Lipinski.NumHDonors(mol)
+  st.write(f'Número de donantes de enlaces de hidrógeno: {num_donors}')
+
+  num_rotatable_bonds = Lipinski.NumRotatableBonds(mol)
+  st.write(f'Número de enlaces giratorios: {num_rotatable_bonds}')
+
+  num_aliphatic_rings = Lipinski.NumAliphaticRings(mol)
+  st.write(f'Número de anillos alifáticos: {num_aliphatic_rings}')
+
+  num_aromatic_rings = Lipinski.NumAromaticRings(mol)
+  st.write(f'Número de anillos aromáticos: {num_aromatic_rings}')
+
+  fraction_csp3 = Lipinski.FractionCSP3(mol)
+  st.write(f'Proporción de átomos de carbono híbrido SP3: {fraction_csp3}')
+
+  inde_flex = round(Crippen.MolMR(mol),4)
+  st.write(f'Índice de refracción molar: {inde_flex}')
+
+  mol_log_p = round(Descriptors.MolLogP(mol),4)
+  st.write(f'Coeficiente de partición grasa-agua: {mol_log_p}')
+
+  tpsa = round(Descriptors.TPSA(mol), 4)
+  st.write(f'Área de superficie del polo topológico: {tpsa}')
 
 
